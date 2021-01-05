bb43bb80d1667fdf6f3fc74f7067bae98c70e98f
Newbie054 <64598990+Newbie054@users.noreply.github.com>
2021-01-05 15:36:17 +0900

    finally done

--- js/register.js
+++ js/register.js
@@ -1,46 +1,62 @@
 function validateForm(event) {
-    event.preventDefault();
-    var email = document.getElementById["email-address"].value;
-    var password = document.getElementById["password"].value;
-    var address = document.getElementById["Address"].value;
-    var DOB= document.getElementById["DOB"].value;
-    var gender_checker = document.getElementById["gender"];
+    var gender_checker = document.getElementsByName("gender");
     var gender = null;
-    var checkbox = document.getElementById["checkbox"].checked;
+    var checkbox = document.getElementById("agree").checked;
+
     for (let i = 0; i < gender_checker.length; i++) {
-        if(element = gender_checker[i].checked){
+        if(gender_checker[i].checked){
             gender = gender_checker[i].value;
             break;
         }
     }
     if(gender == null){
-        alert("please choose your gender");
+        event.preventDefault();
+        return(alert("please choose your gender"));
     }
-    if(DOB==null){
-        alert("please fill the DOB");
+
+    if(document.getElementById("DOB").value==""){
+        event.preventDefault();
+        return(alert("please fill the DOB"));
     }
-    if(password == null || email == null){
-        alert("fill in the email or password");
+    var DOB= document.getElementById("DOB").value;
+
+    if(document.getElementById("password").value == null || document.getElementById("email-address").value == null){
+        event.preventDefault();
+        return(alert("fill in the email or password"));
     }
+    var email = document.getElementById("email-address").value;
+    var password = document.getElementById("password").value;
+
+    if(document.getElementById("Address").value==null){
+        event.preventDefault();
+        return(alert("please fill the Address"));
+    }
+    var address = document.getElementById("Address").value;
+
     if(checkbox == false){
-        alert("read and agree to our TOC first");
+        event.preventDefault();
+        return(alert("read and agree to our TOC first"));
     }
-    else {
 
-        var parts = email.split('@');
-        var domain = parts[1];
+    if (email.indexOf('@') == -1) {
+        event.preventDefault();
+        return(alert('Not a valid e-mail address'));
+    }
 
-        if (domain[0] == '.') {
-            alert('Not a valid e-mail address');
-        }
-        else{
+    var parts = email.split('@');
+    var domain = parts[1];
 
-            var domainParts = domain.split('.');
-            var ext = domainParts[1];
+    if (domain.indexOf('.') == -1) {
+        event.preventDefault();
+        return(alert('Not a valid e-mail address'));
+    }
+    else{
+        var domainParts = domain.split('.');
+        var ext = domainParts[1];
 
-            if (ext.length > 4 || ext.length < 2) {
-                alert('Not a valid e-mail address');
-            }
+        if (ext.length > 4 || ext.length < 2) {
+            event.preventDefault();
+            return(alert('Not a valid e-mail address'));
         }
     }
 }
